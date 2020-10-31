f5a84bc19b2dc5128c9c60b76761ad9f2bf6c2a8
manaswigowda <55323850+manaswigowda@users.noreply.github.com>
2020-10-31 22:17:21 +0530

    Add files via upload

---
+++ src/main/java/com/example/miwok_workshop/phrases.java
@@ -0,0 +1,34 @@
+package com.example.miwok_workshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhrasesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_Phrases);
+
+        final List<MiwokModel> phrases= new ArrayList<>();
+        phrases.add(new MiwokModel("where are you going?","minto wuksus"));
+        phrases.add(new MiwokModel("what is your name?","tinne oyaase'ne"));
+        phrases.add(new MiwokModel("My name is","oyaaset..."));
+        phrases.add(new MiwokModel("How are you feeling?","michekse?"));
+        phrases.add(new MiwokModel("I'm feeling good.","kuchi achit"));
+        phrases.add(new MiwokModel("Are you coming?","eenes'aa?"));
+        phrases.add(new MiwokModel("Yes,I'm coming.","hee'eenem"));
+        phrases.add(new MiwokModel("I'm coming.","eenem"));
+        phrases.add(new MiwokModel("let's go","yoowutis"));
+        phrases.add(new MiwokModel("Come here.","enni'nem"));
+
+        RecyclerView phrases_recyclerView = findViewById(R.id.recyckerView4);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        phrases_recyclerView.setLayoutManager(layoutManager);
+
+        MiwokAdapter miwokAdapter=new MiwokAdapter(this,phrases, R.color.category_phrases);
+        phrases_recyclerView.setAdapter(miwokAdapter);
+
+    }
+}
